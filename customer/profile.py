f70e893f218b0ab816cb8239253882815bbd817f
Chris Marshall <chmarshall@merkleinc.com>
2021-04-04 12:19:31 +0100

    Add additional deployment stages with manual approval. Add explicity api gateway with cors configuration.

--- customer/profile.py
+++ customer/profile.py
@@ -1,9 +1,10 @@
 import json
 from aws_lambda_powertools import Logger
+from aws_lambda_powertools.logging import correlation_paths
 
 logger = Logger()
 
-@logger.inject_lambda_context
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
 def handler(event, context):
     logger.info(event)
 
